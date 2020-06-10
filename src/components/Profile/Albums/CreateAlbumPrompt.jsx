008a247ec8d128eea6d1cc1cca0a69fde0fc787c
spiridon209 <sivand888@gmail.com>
2020-06-10 19:38:01 +0500

    Replace prompt with modal, fix message error

---
+++ src/components/Profile/Albums/CreateAlbumPrompt.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Modal, Input, message } from 'antd';
+import PropTypes from 'prop-types';
+
+const CreateAlbumPrompt = ({ visible, onCreate, onCancel }) => {
+  const [albumName, setAlbumName] = useState('');
+  const chandeHandler = evt => {
+    setAlbumName(evt.target.value);
+  };
+  return (
+    <Modal
+      visible={visible}
+      title="Новый альбом"
+      okText="Создать"
+      cancelText="Отмена"
+      onCancel={onCancel}
+      onOk={() => {
+        if (albumName === '') {
+          message.error('Необходимо указать название альбома');
+          return;
+        }
+        onCreate(albumName);
+        setAlbumName('');
+        onCancel();
+        message.success('Альбом успешно создан!');
+      }}
+    >
+      <Input value={albumName} onChange={chandeHandler} />
+    </Modal>
+  );
+};
+
+CreateAlbumPrompt.propTypes = {
+  visible: PropTypes.bool.isRequired,
+  onCreate: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+export default CreateAlbumPrompt;
